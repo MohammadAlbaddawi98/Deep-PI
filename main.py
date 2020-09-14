aa0f5fdf0e1155d0fe55040c0b2fd53660386d2a
Mohammad Albaddawi <mohammedalbadawy98@gmail.com>
2020-09-14 14:16:22 +0300

    load models and calculate EAR

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Deep pi
 
+
+##import nessesary libraries
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -11,3 +13,17 @@ import time
 import dlib
 import cv2
 import os
+############################################
+# calculate eye aspect ratio (EAR)
+def EAR(eye):
+    p14 = dist.euclidean(eye[0], eye[3])
+    p32 = dist.euclidean(eye[1], eye[5])
+    p65 = dist.euclidean(eye[2], eye[4])
+    Ear = (p65 + p32) / (p14 + p14)
+    return Ear
+#############################################
+#load models
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  #for detect faces
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')  #for detect shap of eyes
+
+#############################################
